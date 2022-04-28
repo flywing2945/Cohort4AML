07298ae5cb285d6121b41a85e7936bbfbbf547d8
unknown <flywing2945@gmail.com>
2022-04-28 12:33:03 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+![logo](HSBClogo2.png)
+
+# This is the Title
+## About this repository
+This is a repository of HSBC Data Science.
+
+<br>
+
+1. DS
+2. Test002
+
---
+++ simple.py
@@ -0,0 +1,4 @@
+# This is a very simple Python File that prints hellow world!. BTW # in Python means to commit code block.
+
+print("hello world!")
+print("This is my first Python code.")
